31ccab7cf1b77d4d9ba521ead104918a4cab78ac
y2nd <y2nd@github.com>
2022-07-15 18:57:38 +0200

    some routes to fetch data

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Nav from '../components/Nav'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -14,6 +15,7 @@ export default function Home() {
       {/* Header */}
       <Header />
       {/* Nav */}
+      <Nav />
 
       {/* Results */}
     </div>
